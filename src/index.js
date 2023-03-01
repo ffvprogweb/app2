a62e854afd788d56ea4e046397f313471672ce6e
ffvprogweb <disclabes@gmail.com>
2023-03-01 10:29:47 -0300

    Changes to be committed: 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    <div></div>
+    <div> teste de div </div>
   </React.StrictMode>
 );
 
